a06a849d9feca3db653ec6612e8faa6620430fb0
Wargh <nafoizret@web.de>
2022-07-14 20:17:55 +0200

    This is still not correct, but it looks better

--- STROOP/Resources/Shaders/Circles.frag.glsl
+++ STROOP/Resources/Shaders/Circles.frag.glsl
@@ -9,7 +9,8 @@ layout(location=0) out vec4 color;
 void main()
 {
 	float f;
-	float delta = fwidth(fs_texCoord).x;
+	vec3 del = max(abs(dFdx(fs_texCoord)), abs(dFdy(fs_texCoord)));
+	float delta = min(del.x, del.y);
 	float outline = fs_outline_color.a;
 	
 	if (fs_texCoord.z == 1.0) {
@@ -22,7 +23,7 @@ void main()
 		f = length(fs_texCoord);
 	}
 
-	float d = clamp((1 - f) / delta, 0.0, 1.0);
+	float d = clamp((1 - (f - delta * outline * 0.5)) / delta, 0.0, 1.0);
 	float d2 = clamp((1 - (f + delta * outline * 0.5)) / delta, 0.0, 1.0);
 	
 	color = mix(vec4(fs_outline_color.rgb, 1), fs_color, d2);
