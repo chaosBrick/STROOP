662ed871287a62314cd1b8b3795dca15572ded52
chaosBrick <1663221+chaosBrick@users.noreply.github.com>
2023-02-28 13:52:44 +0100

    Add IsInsideRect utilitiy function

---
+++ STROOP/Utilities/DrawingUtilities.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace STROOP.Utilities
+{
+    public static class DrawingUtilities
+    {
+        public static bool IsInsideRect(this Point p, Rectangle rectangle) =>
+            p.X >= rectangle.Left && p.X <= rectangle.Right && p.Y >= rectangle.Top && p.Y <= rectangle.Bottom;
+    }
+}
